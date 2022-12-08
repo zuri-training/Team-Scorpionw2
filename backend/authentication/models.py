750a14e6604aaf24a069c7eecd92fed2756cc406
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2022-12-08 18:48:55 +0100

    duplicate folder removed

--- backend/authentication/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomUser(AbstractUser):
-    phone_no = models.CharField(max_length=20)
--- backend/web_builder/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebBuilderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'web_builder'
