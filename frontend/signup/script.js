c90d4c260dba81f9602094c1c4a21b8e147adedf
unknown <emmizychuks@gmail.com>
2022-12-11 16:05:41 +0100

    initial signup commit

---
+++ frontend/signup/script.js
@@ -0,0 +1,24 @@
+// const registerForm = document.getElementById('register');
+
+
+// function register(event) {
+//   event.preventDefault();
+//   console.log('event.target.firstname.value');
+
+//    let firstname = event.target.firstname.value;
+//   let lastname = event.target.lastname.value;
+//   let email = event.target.email.value;
+//   let PhoneNumber = event.target.PhoneNumber.value;
+//   let password = event.target.password.value;
+//   let confirmPassword = event.target.confirmPassword.value;
+
+// }
+
+// let userObj = {
+//   firstname,
+//   lastname,
+//   email,
+//   PhoneNumber,
+//   password,
+//   confirmPassword,
+// };
