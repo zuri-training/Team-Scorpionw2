ed43e9fbf96631cf801eff348c7b59a13412b974
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2022-12-12 12:40:57 +0100

    landing page integrated

--- my_cms/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-#import views
-from.import views
-
-urlpatterns = [
-    path('',views.home,name='home'),
-]
--- my_cms/my_cms/urls.py
+++ my_cms/my_cms/urls.py
@@ -18,8 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('account.urls')),
     path("accounts/", include("authentication.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("home/", include("pages.urls")),
+    path("", include("pages.urls")),
 ]
