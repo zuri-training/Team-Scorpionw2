c1cde0cc2c69f2a1ff2b97911ff63b6f83baccbc
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2022-12-14 09:44:33 +0100

    about page integrated

--- my_cms/pages/urls.py
+++ my_cms/pages/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import HomePageView, DashboardPageView
+from .views import (
+    HomePageView,
+    DashboardPageView,
+    AboutPageView,
+)
 
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("dashboard/", DashboardPageView.as_view(), name="dashboard")
+    path("dashboard/", DashboardPageView.as_view(), name="dashboard"),
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
--- my_cms/pages/views.py
+++ my_cms/pages/views.py
@@ -8,6 +8,10 @@ class HomePageView(TemplateView):
     template_name = "home.html"
 
 
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+
 class DashboardPageView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard.html"
     redirect_field_name = 'login'
