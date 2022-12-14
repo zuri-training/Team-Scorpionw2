e6e20e3aaef3b84431e6167e35c247541d1e775d
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2022-12-14 20:30:13 +0100

    create website page added

--- my_cms/website_build/urls.py
+++ my_cms/website_build/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
     TemplatePageView,
+    WebsiteCreateView,
 )
 
 
 urlpatterns = [
     path("template/", TemplatePageView.as_view(), name="template"),
+    path('create', WebsiteCreateView.as_view(), name='create'),
 ]
--- my_cms/website_build/views.py
+++ my_cms/website_build/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Website
+from django.urls import reverse_lazy
+from django.views.generic import (
+	ListView,
+	DetailView,
+    TemplateView,
+	)
+from django.views.generic.edit import(
+	CreateView,
+	UpdateView,
+	DeleteView,
+	)
+
+from .forms import WebsiteForm
+
 
 class TemplatePageView(TemplateView):
     template_name = "templates.html"
+
+
+class WebsiteCreateView(CreateView):
+	model = Website
+	form_class = WebsiteForm
+	template_name = 'create.html'
+	# fields = [
+    #     "user",
+    #     "title",
+    #     "occupation",
+    #     "about_me",
+    #     "skill1",
+    #     "skill2",
+    #     "skill3",
+    #     "skill4",
+    #     "fb",
+    #     "twitter",
+    #     ]
+
